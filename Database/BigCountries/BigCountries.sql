697f9cee552c143a01a88a3089562efa8e90246b
ejiu <blackfan@163.com>
2018-02-13 17:35:23 +0800

    mysql BigCountries

---
+++ Database/BigCountries/BigCountries.sql
@@ -0,0 +1,22 @@
+SELECT name, population, area 
+FROM World 
+WHERE population >= 25000000 OR area >= 3000000
+;
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area > 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population > 25000000
+;
+-- Faster a little bit
