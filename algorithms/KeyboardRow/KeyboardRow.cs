7745dce1889bb48b075026c5fc4d3183bdfff5fb
ejiu <blackfan@163.com>
2018-02-22 17:44:49 +0800

    keyboardRow

--- algorithms/KeyboardRow/KeyboardRow.cs
+++ algorithms/KeyboardRow/KeyboardRow.cs
@@ -8,72 +8,70 @@ namespace KeyboardRow
 {
     public class Solution
     {
-        //public string[] FindWords(string[] words)
-        //{
-        //    List<string> res = new List<string>();
-        //    HashSet<char> hash1 = new HashSet<char>();
-        //    HashSet<char> hash2 = new HashSet<char>();
-        //    HashSet<char> hash3 = new HashSet<char>();
-        //    char[] arr1 = new char[] { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P' };
-        //    char[] arr2 = new char[] { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L' };
-        //    char[] arr3 = new char[] { 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Z', 'X', 'C', 'V', 'B', 'N', 'M' };
-        //    foreach (var i in arr1)
-        //    {
-        //        hash1.Add(i);
-        //    }
-        //    foreach (var i in arr2)
-        //    {
-        //        hash2.Add(i);
-        //    }
-        //    foreach (var i in arr3)
-        //    {
-        //        hash3.Add(i);
-        //    }
+        public string[] FindWords(string[] words)
+        {
+            List<string> res = new List<string>();
+            HashSet<char> hash1 = new HashSet<char>();
+            HashSet<char> hash2 = new HashSet<char>();
+            HashSet<char> hash3 = new HashSet<char>();
+            char[] arr1 = new char[] { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P' };
+            char[] arr2 = new char[] { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L' };
+            char[] arr3 = new char[] { 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Z', 'X', 'C', 'V', 'B', 'N', 'M' };
+            foreach (var i in arr1)
+            {
+                hash1.Add(i);
+            }
+            foreach (var i in arr2)
+            {
+                hash2.Add(i);
+            }
+            foreach (var i in arr3)
+            {
+                hash3.Add(i);
+            }
 
-        //    foreach (var i in words)
-        //    {
-        //        char[] rs1 = i.ToCharArray();
+            foreach (var i in words)
+            {
+                char[] rs1 = i.ToCharArray();
 
-        //        for(int j=0; j < rs1.Length; j++)
-        //        {
-        //            if (!hash1.Contains(rs1[j]))
-        //            {
-        //                break;
-        //            }
-        //            if(j == rs1.Length - 1)
-        //            {
-        //                res.Add(i);
-        //            }
-        //        }
+                for(int j=0; j < rs1.Length; j++)
+                {
+                    if (!hash1.Contains(rs1[j]))
+                    {
+                        break;
+                    }
+                    if(j == rs1.Length - 1)
+                    {
+                        res.Add(i);
+                    }
+                }
 
-        //        for (int j = 0; j < rs1.Length; j++)
-        //        {
-        //            if (!hash2.Contains(rs1[j]))
-        //            {
-        //                break;
-        //            }
-        //            if (j == rs1.Length - 1)
-        //            {
-        //                res.Add(i);
-        //            }
-        //        }
+                for (int j = 0; j < rs1.Length; j++)
+                {
+                    if (!hash2.Contains(rs1[j]))
+                    {
+                        break;
+                    }
+                    if (j == rs1.Length - 1)
+                    {
+                        res.Add(i);
+                    }
+                }
 
-        //        for (int j = 0; j < rs1.Length; j++)
-        //        {
-        //            if (!hash3.Contains(rs1[j]))
-        //            {
-        //                break;
-        //            }
-        //            if (j == rs1.Length - 1)
-        //            {
-        //                res.Add(i);
-        //            }
-        //        }
-        //    }
-        //    return res.ToArray();
-        //}
-
-
+                for (int j = 0; j < rs1.Length; j++)
+                {
+                    if (!hash3.Contains(rs1[j]))
+                    {
+                        break;
+                    }
+                    if (j == rs1.Length - 1)
+                    {
+                        res.Add(i);
+                    }
+                }
+            }
+            return res.ToArray();
+        }
 
     }
 }
