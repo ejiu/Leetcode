2878f41c5662800a7975b064ae333744b03f81b9
ejiu <blackfan@163.com>
2018-02-22 15:05:07 +0800

    NumberComplement

---
+++ algorithms/NumberComplement/NumberComplement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberComplement
+{
+    public class Solution
+    {
+        public int FindComplement(int num)
+        {
+            int  mask = ~0;
+            while ((num & mask) != 0)
+            {
+                mask <<= 1;
+            }
+            return ~mask & ~num;
+        }
+    }
+}
